be08cf2b06a3ecbbfacde7e31b7bb35199865fc7
Victor <victoraweb@gmail.com>
2014-04-11 22:37:09 -0400

    First version

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/freakzoneStore/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/freakzoneStore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/freakzoneStore/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/freakzoneStore/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/freakzoneStore/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/freakzoneStore/system/');
+define('DIR_DATABASE', '/Applications/MAMP/htdocs/freakzoneStore/system/database/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/freakzoneStore/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/freakzoneStore/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/freakzoneStore/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/freakzoneStore/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/freakzoneStore/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/freakzoneStore/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/freakzoneStore/system/logs/');
+define('DIR_CATALOG', '/Applications/MAMP/htdocs/freakzoneStore/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'kzstore');
+define('DB_PREFIX', 'kz_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/freakzoneStore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/freakzoneStore/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/freakzoneStore/catalog/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/freakzoneStore/system/');
+define('DIR_DATABASE', '/Applications/MAMP/htdocs/freakzoneStore/system/database/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/freakzoneStore/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/freakzoneStore/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/freakzoneStore/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/freakzoneStore/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/freakzoneStore/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/freakzoneStore/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/freakzoneStore/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'kzstore');
+define('DB_PREFIX', 'kz_');
+?>
